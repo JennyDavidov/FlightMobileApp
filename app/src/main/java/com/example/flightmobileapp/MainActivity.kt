213d8f9a8e687c93365499e28febfa525fb14930
yaelb6 <yaelberman6@gmail.com>
2020-06-17 17:50:21 +0300

    show image

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flightmobileapp
 
+import Api
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -7,7 +8,14 @@ import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -100,14 +108,32 @@ class MainActivity : AppCompatActivity() {
 //                Toast.makeText(this, editTextTextPersonName10.text.toString() + " Added to database",
 //                    Toast.LENGTH_LONG
 //                ).show()
+                val context = this.baseContext;
+                val givenUrl = editTextTextPersonName10.text.toString();
                 editTextTextPersonName10.text.clear()
-                openActivity(this)
+                //checking connection with server(given URL)
+                val gson = GsonBuilder().setLenient().create()
+                val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:59669/")
+                    .addConverterFactory(GsonConverterFactory.create(gson)).build()
+                val api = retrofit.create(Api::class.java)
+                val body = api.getImg().enqueue(object : Callback<ResponseBody> {
+                    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>)
+                    {
+                        openActivity(context, givenUrl)
+                    }
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        Toast.makeText(context, givenUrl + " Invalid URL, try again",
+                        Toast.LENGTH_LONG).show()
+                    }
+            })
             }
             updateListOfLocals(dbHandler)
         }
     }
-    fun openActivity(context: Context) {
-        val intent = Intent(this, ControlScreen::class.java)
+    fun openActivity(context: Context, url: String) {
+        val intent = Intent(context, ControlScreen::class.java).apply {
+            putExtra("givenUrl", url);
+        }
         startActivity(intent)
     }
 
