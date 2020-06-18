9cdbca28b2d3b3874260e49fc61b08956de5f12b
yaelb6 <yaelberman6@gmail.com>
2020-06-18 15:31:26 +0300

    post request

--- app/src/main/java/com/example/flightmobileapp/Api.kt
+++ app/src/main/java/com/example/flightmobileapp/Api.kt
@@ -1,7 +1,38 @@
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Json
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+data class CommandObject(
+    @Json(name = "aileron") var aileron: Double,
+    @Json(name = "rudder") var rudder: Double,
+    @Json(name = "elevator") var elevator: Double,
+    @Json(name = "throttle") var throttle: Double
+)
+val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl("http://10.0.2.2:59669/").build()
+object TodoApi {
+    val retrofitService: Api by lazy {
+        retrofit.create(Api ::class.java)
+    }
+}
+var viewModelJob = Job()
+val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 interface Api {
     @GET("/screenshot")
     fun getImg(): Call<ResponseBody>
+    @POST()
+    fun setJoystickValues(@Body updateCommand: CommandObject): Deferred<ResponseBody>
 }
--- app/src/main/java/com/example/flightmobileapp/ControlScreen.kt
+++ app/src/main/java/com/example/flightmobileapp/ControlScreen.kt
@@ -1,6 +1,8 @@
 package com.example.flightmobileapp
 
 import Api
+import CommandObject
+import TodoApi.retrofitService
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.SeekBar
@@ -9,14 +11,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
 import com.jackandphantom.joystickview.JoyStickView
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 import kotlinx.android.synthetic.main.activity_control_screen.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import okhttp3.ResponseBody
+import retrofit
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import uiScope
 
 
 class ControlScreen : AppCompatActivity() {
@@ -26,20 +38,25 @@ class ControlScreen : AppCompatActivity() {
     var maxThrottle = 1
     var stepRudder = 0.1
     var stepThrottle = 0.1
-    private lateinit var url: String;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control_screen)
+        lateinit var command: CommandObject
+        //initialize command object
+        command.aileron = 0.0
+        command.elevator = 0.0
+        command.rudder = 0.0
+        command.throttle = 0.0
         showImage()
-//        url = "http://10.0.2.2:5000/screenshot";
-//        Glide.with(simulator_img.context).load(url).into(simulator_img);
         seekBar1.max = ((maxRudder - minRudder) / stepRudder).toInt();
         seekBar2.max = ((maxThrottle - minThrottle) / stepThrottle).toInt();
         seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                val value: Float = (minRudder + (i * stepRudder)).toFloat()
+                val value: Double = (minRudder + (i * stepRudder)).toDouble()
+                command.rudder = value
                 // Display the current progress of SeekBar
                 textView1.text = "Rudder: $value"
+                postfunction(command)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
             }
@@ -48,9 +65,11 @@ class ControlScreen : AppCompatActivity() {
         })
         seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                val value: Float = (minThrottle + (i * stepThrottle)).toFloat()
+                val value: Double = (minThrottle + (i * stepThrottle)).toDouble()
+                command.throttle = value
                 // Display the current progress of SeekBar
                 textView2.text = "Throttle: $value"
+                postfunction(command)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {
             }
@@ -58,15 +77,41 @@ class ControlScreen : AppCompatActivity() {
             }
         })
         val joystick: JoystickView = findViewById(R.id.joyStickView) as JoystickView
+        var aileron: Double
+        var elevator: Double
+        var prevAileron: Double
+        var prevElevator: Double
+        var newY: Int
+        var aileronString: String
+        var elevatorString: String
         joystick.setOnMoveListener(object : JoystickView.OnMoveListener {
             override fun onMove(angle: Int, strength: Int) {
+                prevAileron = command.aileron
+                prevElevator = command.elevator
+                newY = 100 - joystick.normalizedX
+                aileron = newY.toDouble()
+                aileron = (((aileron/100)*2)-1)
+                command.aileron = aileron
+                elevator = joystick.normalizedY.toDouble()
+                elevator = (((elevator/100)*2)-1)
+                command.elevator = elevator
+                aileronString = String.format("%.2f", aileron)
+                elevatorString = String.format("%.2f", elevator)
+                textView3.text = "Aileron: " + aileronString
+                textView4.text = "Elevator: " + elevatorString
+                if (elevator - prevElevator >= 0.02) {
+                    postfunction(command)
+                } else if (aileron - prevAileron >= 0.02) {
+                    postfunction(command)
+                }
             }
         })
     }
     fun showImage() {
         val gson = GsonBuilder().setLenient().create()
         val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:59669/")
-            .addConverterFactory(GsonConverterFactory.create(gson)).build()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
         val api = retrofit.create(Api::class.java)
         val body = api.getImg().enqueue(object : Callback<ResponseBody> {
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>)
@@ -81,5 +126,17 @@ class ControlScreen : AppCompatActivity() {
             }
         })
     }
-
+    fun postfunction(commandObject: CommandObject) {
+        uiScope.launch {
+            var deferredResults = TodoApi.retrofitService.setJoystickValues(commandObject)
+            try {
+                Toast.makeText(this@ControlScreen, "Set successful",
+                    Toast.LENGTH_LONG).show()
+                var response: ResponseBody= deferredResults.await()
+            } catch (e: Exception) {
+                Toast.makeText(this@ControlScreen, "Error in server",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
